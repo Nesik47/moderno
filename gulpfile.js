6138fda632d0b7bb48ef304a4a1507cd64136326
Nesik47 <nestor12081993@gmail.com>
2023-03-03 21:12:17 -0500

    start-project

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+let gulp = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+let rename = require('gulp-rename');
+let browserSync = require('browser-sync');
+let autoprefixer = require('gulp-autoprefixer');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let cssmin = require('gulp-cssmin')
+/* Шукаєм файл style.scss, переіменовуєм на style.min та виводимр в папку css файл style.min.css */
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename({suffix : '.min'}))
+    .pipe(autoprefixer({
+      overrideBrowserslist:['last 8 versions']
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+/* Створення файлу libs.min.js, його мініфікаціяя та добавляння в нього бібліотек
+ */
+gulp.task('script', function(){
+  return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+});
+
+/* Створення файлу libs.css, його мініфікаціяя та добавляння в нього бібліотек
+ */
+gulp.task('style', function(){
+  return gulp.src([
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/magnific-popup/dist/magnific-popup.css',
+  ])
+    .pipe(concat('libs.min.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('html', function(){
+  return gulp.src('app/*.html')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function(){
+  return gulp.src('app/js/*.js')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "app/"
+      }
+  });
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+  gulp.watch('app/*.html', gulp.parallel('html'));
+  gulp.watch('app/js/*.js', gulp.parallel('js'));
+});
+
+gulp.task('default', gulp.parallel('sass', 'watch', 'browser-sync', 'script', 'style'))
+
